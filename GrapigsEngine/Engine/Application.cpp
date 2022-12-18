62778ba2141b49f77a23a3384d1c2cc2bb2e40a2
Jinwoo Choi <imjinwoo98@gmail.com>
2022-12-18 06:51:34 -0800

    mainscreenshots

--- GrapigsEngine/Engine/Application.cpp
+++ GrapigsEngine/Engine/Application.cpp
@@ -69,6 +69,7 @@ Application::Application(int width, int height, const char* title)
 		glViewport(0, 0, width, height);
 		glEnable(GL_DEPTH_TEST);
 		glEnable(GL_BLEND);
+
 		glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 		glEnable(GL_TEXTURE_CUBE_MAP_SEAMLESS);
 		//glEnable(GL_CULL_FACE);
